8f981b5de6931414c51b6aa23044a46eb5aa60a7
Patrick Caldwell <patrick@mobilemagicdevelopers.com>
2012-06-26 11:25:48 -0400

    Added readme (attempting markdown) and started fixing whitespace in the plugin js file.

---
+++ README.md
@@ -0,0 +1,25 @@
+GiantDropdown
+=============
+
+The GiantDropdown jQuery plugin replaces the standard HTML select list with an unordered list (UL)
+that's easily styleable for a custom presentation of selectable items.  It's called the Giant
+Dropdown because it was originally designed to support dropdown items that are very long and
+which expand a dropdown to an unreasonable size.
+
+
+How it Works
+------------
+
+The Giant Dropdown hides your original select list and adds an unordered list in its place.  The
+UL uses the original select element as its backing data store so any events, data validations,
+and postback information should still work exactly as designed without modification.
+
+### Demo
+This repository includes a working sample of how to use the Giant Dropdown.  You can see another
+demo on [my blog](http://dpatrickcaldwell.blogspot.com/2011/06/giantdropdown-jquery-plugin-for-styling.html).
+
+
+Usage
+-----
+
+The plugin is in the [scripts folder](https://github.com/tncbbthositg/GiantDropdown/tree/master/scripts).
--- scripts/jquery.giantdropdown.js
+++ scripts/jquery.giantdropdown.js
@@ -5,106 +5,107 @@
  *  Autopilot Consulting, LLC
  *  Dual licensed under the MIT or GPL Version 2 licenses.
  */
+"use strict";
 
 (function($) {
-    function refreshUl($ul) {
-        $.each($ul.find("li"), function() {
-            var $li = $(this);
-            var option = $li.data("option");
+  function refreshUl($ul) {
+    $.each($ul.find("li"), function() {
+      var $li = $(this);
+      var option = $li.data("option");
 
-            if (option == null)
+      if (option == null)
+        return;
+
+      if (option.selected)
+        $li.addClass("selected");
+
+      else
+        $li.removeClass("selected");
+    });
+  }
+
+  function scrollToFirstSelected($ul) {
+    var $first = $ul.find("li.selected:first");
+    
+    if ($first.length > 0) {
+      var initialPosition = $first.offset().top - $ul.offset().top;
+      $ul.scrollTop(initialPosition);
+    }
+  }
+
+  var methods = {
+    init: function() {
+      return this.each(function() {
+        // not a dropdown
+        if (this.tagName !== "SELECT")
+          return;
+
+        var $dropdown = $(this);
+
+        // already giant-ified
+        if ($dropdown.data("giantDropdown") != null)
+          return;
+          
+        var $ul = $("<ul />");
+        $ul.addClass("giantdropdown");
+        
+        $ul.addClass($dropdown.attr("class"));
+        $ul.attr("style", $dropdown.attr("style"));
+        
+        if ($dropdown.attr("multiple"))
+          $ul.addClass("multiple");
+
+        $dropdown.hide();
+        $dropdown.after($ul);
+
+        $dropdown.data("giantDropdown", $ul);
+
+        $.each($dropdown.find('option, optgroup'), function() {
+          var option = this;
+          var $option = $(option);
+
+          var $li = $("<li />");
+          $li.addClass(option.tagName.toLowerCase());
+          
+          $li.addClass($option.attr("class"));
+          $li.attr("style", $option.attr("style"));
+          
+          if (option.tagName === "OPTGROUP")
+            $li.text(option.label);
+
+          else if (option.tagName === "OPTION") {
+            $li.text(option.text);
+            $li.data("option", option);
+
+            $li.click(function() {
+              if ($dropdown[0].disabled)
                 return;
 
-            if (option.selected)
-                $li.addClass("selected");
+              option.selected = !option.selected;
+              refreshUl($ul);
+            });
+          }
 
-            else
-                $li.removeClass("selected");
+          $ul.append($li);
         });
+
+        refreshUl($ul);
+        scrollToFirstSelected($ul);
+      });
+    },
+
+    scrollToFirstSelected: function() {
+      return this.each(function() {
+        scrollToFirstSelected($(this));
+      });
     }
+  }
 
-    function scrollToFirstSelected($ul) {
-        var $first = $ul.find("li.selected:first");
-        
-        if ($first.length > 0) {
-            var initialPosition = $first.offset().top - $ul.offset().top;
-            $ul.scrollTop(initialPosition);
-        }
-    }
+  $.fn.giantDropdown = function(method) {
+    if (!method || typeof (method) === "object")
+      return methods.init.apply(this);
 
-    var methods = {
-        init: function() {
-            return this.each(function() {
-                // not a dropdown
-                if (this.tagName !== "SELECT")
-                    return;
-
-                var $dropdown = $(this);
-
-				// already giant-ified
-                if ($dropdown.data("giantDropdown") != null)
-                    return;
-					
-                var $ul = $("<ul />");
-                $ul.addClass("giantdropdown");
-				
-				$ul.addClass($dropdown.attr("class"));
-				$ul.attr("style", $dropdown.attr("style"));
-				
-				if ($dropdown.attr("multiple"))
-					$ul.addClass("multiple");
-
-                $dropdown.hide();
-                $dropdown.after($ul);
-
-				$dropdown.data("giantDropdown", $ul);
-
-                $.each($dropdown.find('option, optgroup'), function() {
-                    var option = this;
-					var $option = $(option);
-
-                    var $li = $("<li />");
-                    $li.addClass(option.tagName.toLowerCase());
-					
-					$li.addClass($option.attr("class"));
-					$li.attr("style", $option.attr("style"));
-					
-                    if (option.tagName === "OPTGROUP")
-                        $li.text(option.label);
-
-                    else if (option.tagName === "OPTION") {
-                        $li.text(option.text);
-                        $li.data("option", option);
-
-                        $li.click(function() {
-                            if ($dropdown[0].disabled)
-                                return;
-
-                            option.selected = !option.selected;
-                            refreshUl($ul);
-                        });
-                    }
-
-                    $ul.append($li);
-                });
-
-                refreshUl($ul);
-                scrollToFirstSelected($ul);
-            });
-        },
-
-        scrollToFirstSelected: function() {
-            return this.each(function() {
-                scrollToFirstSelected($(this));
-            });
-        }
-    }
-
-    $.fn.giantDropdown = function(method) {
-        if (!method || typeof (method) === "object")
-            return methods.init.apply(this);
-
-        else
-            return methods[method].apply(this);
-    };
+    else
+      return methods[method].apply(this);
+  };
 })(jQuery);
